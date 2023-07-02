95ecdc436025d03757d79febd2b862261b985d17
Демон Чайный <volshebnuyoleg666@yandex.ru>
2023-07-02 14:16:13 +0400

    обязательно

--- Casino/Program.cs
+++ Casino/Program.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Guy 
+namespace Guy
 {
     class Guy
     {
-        Random random = new Random();
-        double odds = 0.75;
         public string Name;
         public int Cash;
-        public void Welcome()
-        {
-            Console.WriteLine("Welcome to the casino. The odds are" + odds);
-        }
         public void WriteMyInfo()
         {
             Console.WriteLine(Name + " has " + Cash + " bucks.");
@@ -24,42 +18,65 @@ namespace Guy
         {
             if (amount <= 0)
             {
-                Console.WriteLine("You can't bet 0. Please, bet another amount");
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
                 return 0;
             }
             if (amount > Cash)
             {
-                Console.WriteLine("You don't have enough money for this bet");
+                Console.WriteLine(Name + " says: " +
+                "I don't have enough cash to give you " + amount);
                 return 0;
             }
             Cash -= amount;
             return amount;
         }
-        static void Main(string[] args)
-
+        /// <param name="amount">Получаемая сумма.</param>
+        public void ReceiveCash(int amount)
         {
-            Guy player = new Guy() { Cash = 100, Name = "The player" };
-            
-            while (true)
+            if (amount <= 0)
             {
-                player.WriteMyInfo();
-                Console.Write("How much do you want to bet");
-                string howMuch = Console.ReadLine();
-                if (howMuch == "") return;
-                if (int.TryParse(howMuch, out int amount))
-                {
-                    int pot = howMuch.GiveCash(amount);
-                    odds = random.Next(0, 1);
-                    if (odds == 0) return (pot *= 2);
-                    if (odds == 1) return ("Sorry, you lose your money. You have" + pot := 2);
-
-                }
-                else
-                {
-                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
-                }
+                Console.WriteLine(Name + " says: " + amount + " isn't an amount I'll take");
+            }
+            else
+            {
+                Cash += amount;
             }
         }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = .75;
+
+            Guy player = new Guy() { Cash = 100, Name = "The player" };
+
+            Console.WriteLine("Welcome to the casino. The odds are " + odds);
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.Write("How much do you want to bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount) * 2;
+                    if (pot > 0)
+                    {
+                        if (random.NextDouble() > odds)
+                        {
+                            int winnings = pot;
+                            Console.WriteLine("You win" + winnings);
+                            player.ReceiveCash(winnings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose. Please enter a valid number. ");
+                        }
+
+                    }
+                 
+                }
+            }
+        }    
     }
 }
 
